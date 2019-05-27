515f79c5a9f66225077e05bcb30c6ea70b2d4291
ilusi0npl <mateuszciolek@gmail.com>
2019-05-27 19:21:44 +0200

    Clean-up in test classes

--- src/test/java/tests/FailedLoginTests.java
+++ src/test/java/tests/FailedLoginTests.java
@@ -12,13 +12,13 @@ public class FailedLoginTests extends TestBase {
     @Test
     public void asUserTryToLogInWithIncorrectLoginAndPassword() {
 
-        LandingPage landingPage = new LandingPage(driver);
+        LandingPage landingPage = new LandingPage();
         landingPage.clickOnEnterStoreLink();
 
-        TopMenuPage topMenuPage = new TopMenuPage(driver);
+        TopMenuPage topMenuPage = new TopMenuPage();
         topMenuPage.clickOnSignInLink();
 
-        LoginPage loginPage = new LoginPage(driver);
+        LoginPage loginPage = new LoginPage();
         loginPage.typeIntoUserNameField("NotExistingLogin");
         loginPage.typeIntoPasswordField("NotProperPassword");
         loginPage.clickOnLoginButton();
