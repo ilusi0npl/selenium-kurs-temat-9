503c3deef8f5b6cb038df042b5aff077acf4ba5c
ilusi0npl <mateuszciolek@gmail.com>
2019-06-02 09:40:04 +0200

    Updated TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,13 +5,15 @@ import driver.manager.DriverUtils;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import static navigation.ApplicationURLs.APPLICATION_URL;
+
 public class TestBase {
 
     @BeforeMethod
     public void beforeTest() {
         DriverManager.getWebDriver();
         DriverUtils.setInitialConfiguration();
-        DriverUtils.navigateToPage("http://przyklady.javastart.pl/jpetstore/");
+        DriverUtils.navigateToPage(APPLICATION_URL);
     }
 
     @AfterMethod
