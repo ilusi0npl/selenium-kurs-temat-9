0e7d49715680f05cb5a4ebd35e4196ed76b9fc1e
ilusi0npl <mateuszciolek@gmail.com>
2019-06-02 09:40:34 +0200

    Re-factored tests to support relative URL

--- src/test/java/tests/FailedLoginTests.java
+++ src/test/java/tests/FailedLoginTests.java
@@ -1,23 +1,23 @@
 package tests;
 
+import driver.manager.DriverUtils;
 import org.testng.annotations.Test;
-import page.objects.LandingPage;
 import page.objects.LoginPage;
 
+import static navigation.ApplicationURLs.LOGIN_URL;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class FailedLoginTests extends TestBase {
 
     @Test
     public void asUserTryToLogInWithIncorrectLoginAndPassword() {
-        LandingPage landingPage = new LandingPage();
-        landingPage
-                .clickOnEnterStoreLink()
-                .clickOnSignInLink()
+        DriverUtils.navigateToPage(LOGIN_URL);
+
+        LoginPage loginPage = new LoginPage();
+        loginPage
                 .typeIntoUserNameField("NotExistingLogin")
                 .typeIntoPasswordField("NotProperPassword")
                 .clickOnLoginButton();
-        LoginPage loginPage = new LoginPage();
         String warningMessage = loginPage.getWarningMessage();
 
         assertEquals(warningMessage, "Invalid username or password. Signon failed.");
--- src/test/java/tests/PositiveLoginTests.java
+++ src/test/java/tests/PositiveLoginTests.java
@@ -1,19 +1,20 @@
 package tests;
 
+import driver.manager.DriverUtils;
 import org.testng.annotations.Test;
-import page.objects.*;
+import page.objects.LoginPage;
 
+import static navigation.ApplicationURLs.LOGIN_URL;
 import static org.testng.Assert.assertTrue;
 
 public class PositiveLoginTests extends TestBase {
 
     @Test
     public void asUserLoginUsingValidLoginAndPassword() {
+        DriverUtils.navigateToPage(LOGIN_URL);
 
-        LandingPage landingPage = new LandingPage();
-        boolean isBannerAfterLoginDisplayed = landingPage
-                .clickOnEnterStoreLink()
-                .clickOnSignInLink()
+        LoginPage loginPage = new LoginPage();
+        boolean isBannerAfterLoginDisplayed = loginPage
                 .typeIntoUserNameField("j2ee")
                 .typeIntoPasswordField("j2ee")
                 .clickOnLoginButton()
