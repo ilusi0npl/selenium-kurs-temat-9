924192da33a01fc31fad8dcd7389a09c4b9614d7
ilusi0npl <mateuszciolek@gmail.com>
2019-06-02 09:39:56 +0200

    Created ApplicationURLs

---
+++ src/test/java/navigation/ApplicationURLs.java
@@ -0,0 +1,6 @@
+package navigation;
+
+public class ApplicationURLs {
+    public static final String APPLICATION_URL = "http://przyklady.javastart.pl/jpetstore/";
+    public static final String LOGIN_URL = APPLICATION_URL + "actions/Account.action?signonForm=";
+}
