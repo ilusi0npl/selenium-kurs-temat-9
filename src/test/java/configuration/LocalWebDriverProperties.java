12c4ec7fbec3aeebb92fd65a97cf39b6600069d9
ilusi0npl <mateuszciolek@gmail.com>
2019-06-15 11:29:04 +0200

    Clean-up on LocalWebDriverProperties

--- src/test/java/configuration/LocalWebDriverProperties.java
+++ src/test/java/configuration/LocalWebDriverProperties.java
@@ -1,15 +1,7 @@
 package configuration;
 
-import driver.BrowserType;
-
 public class LocalWebDriverProperties {
 
-    // Metody zwracają właściwości dla poszczególnych kluczy, analogicznie jak w przypadku AppProperties
-
-    public static BrowserType getLocalBrowser() {
-        return BrowserType.valueOf(ConfigurationProperties.getProperties().getProperty("local.browser"));
-    }
-    
     public static String getChromeWebDriverLocation() {
         return ConfigurationProperties.getProperties().getProperty("chrome.driver.location");
     }
