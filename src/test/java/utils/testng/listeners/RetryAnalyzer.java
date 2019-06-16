903631a4894cb6315fb5d532ba7aa11c2ea96ce8
ilusi0npl <mateuszciolek@gmail.com>
2019-06-16 17:23:09 +0200

    Created RetryAnalyzer

---
+++ src/test/java/utils/testng/listeners/RetryAnalyzer.java
@@ -0,0 +1,26 @@
+package utils.testng.listeners;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+    private int count = 0;
+    private static final int MAX_NUMBER_OF_RETRIES = 2;
+    private Logger logger = LogManager.getLogger(RetryAnalyzer.class);
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (!iTestResult.isSuccess()) {                      //Sprawdza czy test zakończył się porażką.
+            if (count < MAX_NUMBER_OF_RETRIES) {             //Sprawdzamy czy test został powtórzony. Jeśli tak to nie powtarzamy go ponownie
+                count++;
+                logger.info("Retrying test method {}!", iTestResult.getName());
+                return true;
+            }
+        }
+        logger.info("Test method {} will be not retried!", iTestResult.getName());
+        return false;
+    }
+}
