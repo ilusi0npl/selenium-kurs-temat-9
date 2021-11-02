6e43583430c616b3a1b3762bb4eebea710192dd9
ilusi0n <mateuszciolek@gmail.com>
2021-11-02 19:11:05 +0100

    Test fix

--- src/test/java/tests/ShoppingCartTest.java
+++ src/test/java/tests/ShoppingCartTest.java
@@ -4,8 +4,6 @@ import org.testng.annotations.Test;
 import page.objects.LandingPage;
 import page.objects.LoginPage;
 
-import static org.testng.Assert.assertEquals;
-
 public class ShoppingCartTest extends TestBase {
 
     @Test
@@ -19,8 +17,7 @@ public class ShoppingCartTest extends TestBase {
                 .clickOnAddToCartSmallAngelfish()
                 .clickOnProceedToCheckout();
 
-        String warningMessage = loginPage.getWarningMessage();
-        assertEquals(warningMessage, "You must sign on before attempting to check out. Please sign on and try checking out again.");
+        loginPage.assertThatWarningIsDisplayed("You must sign on before attempting to check out. Please sign on and try checking out again.");
     }
 
 }
